427e8609cda017b1c914ad25fa54adc77d0f9877
snesjhon <snesjhon@gmail.com>
2019-11-06 18:21:57 -0800

    11/6 - breaking rating into its own component

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,41 +1,96 @@
-// https://codesandbox.io/s/xpl2wjpyoq
-
 import * as React from "react";
 import { useState, useCallback } from "react";
 import styled from "styled-components";
 
-const RatingStart = styled.svg<{ index: number }>`
-  display: inline-block;
-  width: 20px;
-  overflow: hidden;
-  direction: ${props => (props.index && props.index % 2 === 0 ? "ltr" : "rtl")};
-`;
-
-type StarProps = {
-  rate: RatingProps["stars"];
-  setRate: React.Dispatch<React.SetStateAction<RatingProps["stars"]>>;
+interface StarProps {
+  index: number;
+}
+const Star = ({ index }: StarProps) => {
+  return <div>star</div>;
 };
 
-const Star = ({ rate, setRate }: StarProps) => {
-  return <div>test</div>;
-};
+interface RatingProps {
+  value: number | 0;
+  onChange: () => void;
+}
 
-type RatingProps = {
-  stars: {
-    star: number | null;
-    tempStar: number | null;
-  };
-};
-const Rating = ({ stars }: RatingProps) => {
-  const [starRating, setStarRating] = useState(stars || null);
+const Rating = ({ value, onChange }: RatingProps) => {
+  const [currentValue, setCurrentValue] = useState(value);
   return (
     <span>
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((e, i) => (
-        <Star key={i} rate={starRating} setRate={setStarRating} />
+        <Star index={i} />
       ))}
     </span>
   );
 };
+
+export default Rating;
+
+// https://codesandbox.io/s/xpl2wjpyoq
+
+// import * as React from "react";
+// import { useState, useCallback } from "react";
+// import styled from "styled-components";
+
+// const RatingStart = styled.svg<{ index: number }>`
+//   display: inline-block;
+//   width: 20px;
+//   overflow: hidden;
+//   direction: ${props => (props.index && props.index % 2 === 0 ? "ltr" : "rtl")};
+// `;
+
+// type StarProps = {
+//   index: number;
+//   rate: RatingProps["stars"];
+//   setRate: React.Dispatch<React.SetStateAction<RatingProps["stars"]>>;
+// };
+
+// const Star = ({ rate, setRate, index }: StarProps) => {
+//   const {star, tempStar} = rate;
+
+//   // if (this.state.rating >= i && this.state.rating !== null) {
+//   //   klass = "ion-ios-star";
+//   // }
+
+//   return (
+// <RatingStart
+// index=
+//   xmlns="http://www.w3.org/2000/svg"
+//   width="24"
+//   height="24"
+//   viewBox="0 0 24 24"
+//   fill="none"
+//   stroke={full ? "blue" : "yellow"}
+//   strokeWidth="2"
+//   strokeLinecap="round"
+//   strokeLinejoin="round"
+//   onMouseEnter={handleMouseEnter}
+//   onMouseLeave={handleMouseLeave}
+// >
+//   <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+// </RatingStart>
+//   );
+// };
+
+// type RatingProps = {
+//   stars: {
+//     star: number | null;
+//     tempStar: number | null;
+//   };
+// };
+// const Rating = ({ stars }: RatingProps) => {
+//   const [starRating, setStarRating] = useState(stars || null);
+//   return (
+//     <span>
+//       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((e, i) => (
+//         <Star key={i} index={i} rate={starRating} setRate={setStarRating} />
+//       ))}
+//     </span>
+//   );
+// };
+
+// export default Rating;
 
 // import * as React from "react";
 // import { useState, useCallback } from "react";
@@ -60,24 +115,24 @@ const Rating = ({ stars }: RatingProps) => {
 //     setOverride
 //   ]);
 //   const handleMouseLeave = useCallback(() => setOverride(null), [setOverride]);
-//   return (
-//     <RatingStart
-//       index={index}
-//       xmlns="http://www.w3.org/2000/svg"
-//       width="24"
-//       height="24"
-//       viewBox="0 0 24 24"
-//       fill="none"
-//       stroke={full ? "blue" : "yellow"}
-//       strokeWidth="2"
-//       strokeLinecap="round"
-//       strokeLinejoin="round"
-//       onMouseEnter={handleMouseEnter}
-//       onMouseLeave={handleMouseLeave}
-//     >
-//       <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
-//     </RatingStart>
-//   );
+// return (
+//   <RatingStart
+//     index={index}
+//     xmlns="http://www.w3.org/2000/svg"
+//     width="24"
+//     height="24"
+//     viewBox="0 0 24 24"
+//     fill="none"
+//     stroke={full ? "blue" : "yellow"}
+//     strokeWidth="2"
+//     strokeLinecap="round"
+//     strokeLinejoin="round"
+//     onMouseEnter={handleMouseEnter}
+//     onMouseLeave={handleMouseLeave}
+//   >
+//     <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+//   </RatingStart>
+// );
 // };
 
 // type RatingProps = {
