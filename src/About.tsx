431c6e501d878e1b4302fb80b3005382c1ab5ab1
snesjhon <snesjhon@gmail.com>
2019-12-17 23:49:49 -0800

    12/17 - cleanup

--- src/About.tsx
+++ src/About.tsx
@@ -6,34 +6,54 @@
  */
 
 import * as React from "react";
+import { useState, useEffect } from "react";
 import { Box, Text, Icon, Flex, Button } from "./components";
-import { useStoreActions } from "./config/store";
+import { useStoreActions, useStoreState } from "./config/store";
 
 const About = () => {
   const userGet = useStoreActions(actions => actions.global.userGet);
-  return (
-    <Box>
-      <Flex flexDirection="column" alignItems="center">
-        <Flex mt={4}>
-          <Icon name="album" mr={3} stroke="orange" />
-          <Icon name="tv" mr={3} stroke="blue" />
-          <Icon name="film" mr={3} stroke="secondary" />
+  const user = useStoreState(state => state.global.user);
+  const [isLoading, setIsLoading] = useState(false);
+
+  console.log(user);
+  useEffect(() => {
+    if (user !== null && isLoading) {
+      setIsLoading(false);
+    }
+  }, [user, isLoading]);
+
+  if (isLoading) {
+    return <div>loading</div>;
+  } else {
+    return (
+      <Box>
+        <Flex flexDirection="column" alignItems="center">
+          <Flex mt={4}>
+            <Icon name="album" mr={3} stroke="orange" />
+            <Icon name="tv" mr={3} stroke="blue" />
+            <Icon name="film" mr={3} stroke="secondary" />
+          </Flex>
+          <Text as="h1" textAlign="center" fontWeight={600} mt={2}>
+            MediaDiary
+          </Text>
         </Flex>
-        <Text as="h1" textAlign="center" fontWeight={600} mt={2}>
-          MediaDiary
+        <Box my={3} borderTop="1px solid #d1d5da" />
+        <Text fontSize={4} textAlign="center">
+          Track All Your Media.
         </Text>
-      </Flex>
-      <Box my={3} borderTop="1px solid #d1d5da" />
-      <Text fontSize={4} textAlign="center">
-        Track All Your Media.
-      </Text>
-      <Flex justifyContent="center" my={4}>
-        <Button variant="secondary" onClick={() => userGet()}>
-          Sign In
-        </Button>
-      </Flex>
-    </Box>
-  );
+        <Flex justifyContent="center" my={4}>
+          <Button variant="secondary" onClick={signIn}>
+            Sign In
+          </Button>
+        </Flex>
+      </Box>
+    );
+  }
+
+  function signIn() {
+    setIsLoading(true);
+    userGet();
+  }
 };
 
 export default About;
