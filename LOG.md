cbfa2df6a925bd2e7df78c1bc7ef93827e06d2f9
snesjhon <snesjhon@gmail.com>
2020-02-29 19:51:26 -0800

    [singleContainer] A lot of configs for netlify

--- LOG.md
+++ LOG.md
@@ -1,11 +1,15 @@
 # Media Diary - Log
 
-2020-02-06
+02/29/20 06:49:21 PM
+
+- Awesome times, I was able to completely use parcel 2 and launch to netlify :D
+
+02/06/20 08:49:53 PM
 
 - Made a lot of progress in trying to switch from the old styled system to Material UI.
 - The biggest hurtle was trying to make sure that the right scrolling is working correctly.
 
-2020-01-21
+01/21/20 10:50:08 AM
 
 - I want to create a way of displaying the "month" row to be static and once it's static it would move as your start scrolling. BUT I also want this row to be virtualized, so it's not always loading the images.
 - Maybe I need the images to be lazy loaded instead? Instead of just loading everything up front. Since the rows aren't really taking up that much data.
--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,6 +1,5 @@
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
-import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
@@ -9,7 +8,6 @@ import Typography from "@material-ui/core/Typography";
 import * as React from "react";
 import { useState } from "react";
 import { useStoreActions, useStoreState } from "./config/store";
-import Navigation from "./Navigation";
 
 const useStyles = makeStyles(theme => ({
   form: {
@@ -49,92 +47,87 @@ function Profile() {
   const [localYear, setLocalYear] = useState(preferences.year);
 
   return (
-    <Container maxWidth="md">
-      <Navigation />
-      <Box borderColor="grey.300" border={1} borderTop={0} p={2}>
-        <Box py={4}>
-          <Grid container spacing={5}>
-            <Grid item xs={5}>
-              <Box textAlign="center">
-                <img
-                  className={classes.avatar}
-                  src={(user !== null && user.photoURL + "=s130-c") || ""}
-                />
-                <Typography component="div">
-                  <Box fontWeight="fontWeightBold">
-                    {user !== null && user.displayName}
-                  </Box>
-                </Typography>
-                <Typography>{user !== null && user.email}</Typography>
+    <Box py={4}>
+      <Grid container spacing={5}>
+        <Grid item xs={5}>
+          <Box textAlign="center">
+            <img
+              className={classes.avatar}
+              src={(user !== null && user.photoURL + "=s130-c") || ""}
+            />
+            <Typography component="div">
+              <Box fontWeight="fontWeightBold">
+                {user !== null && user.displayName}
               </Box>
-            </Grid>
-            <Grid item xs={7}>
-              <Box display="flex" flexDirection="column">
-                <Typography>
-                  <Box component="span" fontWeight="fontWeightBold" mb={2}>
-                    Theme
-                  </Box>
-                </Typography>
-                <Select
-                  className={classes.form}
-                  id="theme"
-                  value={localTheme}
-                  onChange={e => {
-                    const target = e.target.value;
-                    if (target === "light" || target === "dark") {
-                      setLocalTheme(target);
-                    }
-                  }}
-                >
-                  <MenuItem value="light">Light</MenuItem>
-                  <MenuItem value="dark">Dark</MenuItem>
-                </Select>
-                <Box py={2} />
-                <Typography>
-                  <Box component="span" fontWeight="fontWeightBold">
-                    Current Year
-                  </Box>
-                </Typography>
-                <Select
-                  className={classes.form}
-                  value={localYear}
-                  onChange={(
-                    e: React.ChangeEvent<{
-                      name?: string | undefined;
-                      value: string;
-                    }>
-                  ) => setLocalYear(e.target.value)}
-                >
-                  {yearsSelected.map((e, i) => (
-                    <MenuItem key={"yearSelect" + i} value={e}>
-                      {e}
-                    </MenuItem>
-                  ))}
-                </Select>
-                <Box display="flex" pt={5} justifyContent="space-between">
-                  <Button>+ Add New Year</Button>
-                  <Button
-                    size="large"
-                    color="secondary"
-                    variant="contained"
-                    disableElevation
-                    onClick={() =>
-                      userPutPreferences({
-                        theme: localTheme,
-                        year: localYear,
-                        years: yearsSelected
-                      })
-                    }
-                  >
-                    Save
-                  </Button>
-                </Box>
+            </Typography>
+            <Typography>{user !== null && user.email}</Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={7}>
+          <Box display="flex" flexDirection="column">
+            <Typography>
+              <Box component="span" fontWeight="fontWeightBold" mb={2}>
+                Theme
               </Box>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-    </Container>
+            </Typography>
+            <Select
+              className={classes.form}
+              id="theme"
+              value={localTheme}
+              onChange={e => {
+                const target = e.target.value;
+                if (target === "light" || target === "dark") {
+                  setLocalTheme(target);
+                }
+              }}
+            >
+              <MenuItem value="light">Light</MenuItem>
+              <MenuItem value="dark">Dark</MenuItem>
+            </Select>
+            <Box py={2} />
+            <Typography>
+              <Box component="span" fontWeight="fontWeightBold">
+                Current Year
+              </Box>
+            </Typography>
+            <Select
+              className={classes.form}
+              value={localYear}
+              onChange={(
+                e: React.ChangeEvent<{
+                  name?: string | undefined;
+                  value: string;
+                }>
+              ) => setLocalYear(e.target.value)}
+            >
+              {yearsSelected.map((e, i) => (
+                <MenuItem key={"yearSelect" + i} value={e}>
+                  {e}
+                </MenuItem>
+              ))}
+            </Select>
+            <Box display="flex" pt={5} justifyContent="space-between">
+              <Button>+ Add New Year</Button>
+              <Button
+                size="large"
+                color="secondary"
+                variant="contained"
+                disableElevation
+                onClick={() =>
+                  userPutPreferences({
+                    theme: localTheme,
+                    year: localYear,
+                    years: yearsSelected
+                  })
+                }
+              >
+                Save
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
