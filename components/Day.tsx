7772c394622d44a29904e44ab5e2de7d107a6513
Jhon Paredes <snesjhon@gmail.com>
2020-11-03 12:40:25 -0800

    feat(issue79): Add modal fixes since pagination works (#86)
    * feat(issue79): Add modal fixes since pag works
    
    * chore(issue79): forgot I needed js-cookie types

--- components/Day.tsx
+++ components/Day.tsx
@@ -1,25 +1,27 @@
 import {
+  Box,
   Divider,
   Flex,
+  Grid,
+  Heading,
   HStack,
   IconButton,
   Stat,
   StatHelpText,
   StatNumber,
-  //useDisclosure,
+  Text,
+  Image,
 } from "@chakra-ui/core";
 import { EditIcon, InfoOutlineIcon, StarIcon } from "@chakra-ui/icons";
-import { useCollection, useDocument } from "@nandorojo/swr-firestore";
+import { useDocument } from "@nandorojo/swr-firestore";
 import dayjs from "dayjs";
 import { useRouter } from "next/router";
 import React, { useContext } from "react";
 import Rating from "react-rating";
 import { DiaryAdd } from "../config/mediaTypes";
 import { ContextDispatch } from "../config/store";
-import { createMediaState } from "../utils/helpers";
 import useUser from "../utils/useUser";
 import StarEmptyIcon from "./Icons/StartEmptyIcon";
-import Info from "./Info";
 
 function Day({ diaryId }: { diaryId: string }) {
   const { user } = useUser();
@@ -27,69 +29,111 @@ function Day({ diaryId }: { diaryId: string }) {
   const { data } = useDocument<DiaryAdd>(
     user !== null && user ? `${user.email}/${diaryId}` : null
   );
-  //const { isOpen, onToggle } = useDisclosure();
   const router = useRouter();
   if (data) {
-    const { rating, diaryDate, overview } = data;
+    const {
+      rating,
+      diaryDate,
+      artist,
+      title,
+      poster,
+      genre,
+      releasedDate,
+    } = data;
     return (
       <>
-        <Info item={data} />
-        <HStack spacing={2} justify="center" mt={2}>
-          {overview && (
-            <>
-              <IconButton
-                variant="outline"
-                colorScheme="blue"
-                aria-label="Show Overview"
-                size="sm"
-                icon={<InfoOutlineIcon />}
-                //onClick={onToggle}
-                isRound
-              />
-            </>
-          )}
-          <IconButton
-            icon={<EditIcon />}
-            aria-label="edit"
-            variant="outline"
-            size="sm"
-            colorScheme="green"
-            isRound
-            onClick={() => {
-              dispatch({
-                type: "edit",
-                payload: {
-                  diaryId,
-                  diary: data,
-                },
-              });
-              router.push("/home/?view=edit", "/edit", {
-                shallow: true,
-              });
-            }}
-          />
-        </HStack>
-        <Divider mt={3} mb={2} />
-        <Flex justifyContent="space-between">
-          <Stat>
-            <StatNumber>
-              {dayjs(diaryDate.toDate()).format("MMM D, YYYY")}
-            </StatNumber>
-            <StatHelpText>Date</StatHelpText>
-          </Stat>
-          <Stat>
-            <StatNumber>
-              <Rating
-                fractions={2}
-                readonly
-                initialRating={rating}
-                fullSymbol={<StarIcon color="purple.400" />}
-                emptySymbol={<StarEmptyIcon stroke="purple.400" />}
-              />
-            </StatNumber>
-            <StatHelpText textAlign="right">Rating</StatHelpText>
-          </Stat>
+        <Flex
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+          textAlign="center"
+        >
+          <Text fontSize="lg">{artist}</Text>
+          <Heading
+            fontWeight="bold"
+            fontStyle="italic"
+            size="lg"
+            lineHeight={1.3}
+          >
+            {title}
+          </Heading>
         </Flex>
+        <Grid
+          gridTemplateColumns="0.6fr 0.4fr"
+          gridGap="1.5rem"
+          justifyContent="center"
+          mt={6}
+        >
+          <Box>
+            <Image
+              src={poster}
+              w="13rem"
+              borderRadius="5px"
+              border="1px solid"
+              borderColor="gray.300"
+              loading="eager"
+            />
+          </Box>
+          <Flex flexDirection="column" justifyContent="space-around">
+            <Box>
+              <Text fontWeight={500} fontSize="sm">
+                Date
+              </Text>
+              <Text fontWeight="bold" fontSize="lg">
+                {dayjs(diaryDate.toDate()).format("MMM D, YYYY")}
+              </Text>
+            </Box>
+            <Box>
+              <Text fontWeight={500} fontSize="sm">
+                Rating
+              </Text>
+              <Text fontWeight="bold">
+                <Rating
+                  fractions={2}
+                  readonly
+                  initialRating={rating}
+                  fullSymbol={<StarIcon color="purple.400" w="20px" h="20px" />}
+                  emptySymbol={
+                    <StarEmptyIcon stroke="purple.400" w="20px" h="20px" />
+                  }
+                />
+              </Text>
+            </Box>
+            <Box>
+              <Text fontWeight={500} fontSize="sm">
+                Edit
+              </Text>
+              <IconButton
+                icon={<EditIcon />}
+                aria-label="edit"
+                variant="outline"
+                size="sm"
+                colorScheme="green"
+                isRound
+                onClick={() => {
+                  dispatch({
+                    type: "edit",
+                    payload: {
+                      diaryId,
+                      diary: data,
+                    },
+                  });
+                  router.push("/home/?view=edit", "/edit", {
+                    shallow: true,
+                  });
+                }}
+              />
+            </Box>
+          </Flex>
+        </Grid>
+        <Text fontSize="xs" color="gray.400" mt={1}>
+          {genre && <>{genre} • </>}
+          {typeof releasedDate !== "undefined" &&
+            `${new Date(releasedDate).toLocaleDateString("en-us", {
+              year: "numeric",
+            })}`}
+        </Text>
+        <Divider mt={3} mb={2} />
       </>
     );
   }
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,16 +1,26 @@
-import { Button, CircularProgress, Flex, Grid, Spinner } from "@chakra-ui/core";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Text,
+  Flex,
+  Grid,
+  Spinner,
+} from "@chakra-ui/core";
 import { useRouter } from "next/router";
 import React from "react";
 import Day from "../components/Day";
-import LogEdit from "../components/LogEdit";
 import Header from "../components/Header";
+import LogoIcon from "../components/Icons/LogoIcon";
 import Layout from "../components/Layout";
-import LayoutModal from "../components/LayoutModal";
 import Log from "../components/Log";
+import LogEdit from "../components/LogEdit";
 import MediaDiary from "../components/MediaDiary";
 import Search from "../components/Search";
 import useUser from "../utils/useUser";
-import LogoIcon from "../components/Icons/LogoIcon";
 
 function App() {
   const { user } = useUser();
@@ -36,11 +46,35 @@ function App() {
           {!!router.query.search && <Search />}
           {!!router.query.log && <Log />}
           {!!router.query.view && router.query.view === "edit" && <LogEdit />}
-          {!!router.query.day && (
-            <LayoutModal>
-              <Day diaryId={router.query.day.toString()} />
-            </LayoutModal>
-          )}
+          <Drawer
+            onClose={() => router.push("/home")}
+            isOpen={!!router.query.day}
+            size="full"
+          >
+            <DrawerOverlay zIndex={2}>
+              <DrawerContent>
+                <DrawerHeader>
+                  <Flex align="center">
+                    <LogoIcon boxSize={5} mr={1} />
+                    <Text
+                      fontSize={{ base: "md", md: "xl" }}
+                      color="purple.700"
+                      fontWeight="medium"
+                      cursor="pointer"
+                    >
+                      mediaDiary
+                    </Text>
+                  </Flex>
+                </DrawerHeader>
+                <DrawerCloseButton />
+                <DrawerBody>
+                  {!!router.query.day && (
+                    <Day diaryId={router.query.day.toString()} />
+                  )}
+                </DrawerBody>
+              </DrawerContent>
+            </DrawerOverlay>
+          </Drawer>
         </>
       )}
     </Layout>
