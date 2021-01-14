e89d74ae05c4b3bb326ec42ac9993fdf7951703a
Jhon Paredes <snesjhon@gmail.com>
2021-01-14 12:43:04 -0800

    feat(760f): Add better language to loggedBefore feat (#164)
    * feat(760f): Add better language to loggedBefore feat
    
    * chore(760f): Fixed the build issue with undef mediaInfo

--- src/config/contants.ts
+++ src/config/contants.ts
@@ -1,3 +1,9 @@
 import type { MediaTypes } from "./types";
 
 export const MEDIA_TYPES: MediaTypes[] = ["movie", "tv", "album"];
+
+export const MEDIA_LOGGED_BEFORE: { [key in MediaTypes]: string } = {
+  album: "I've heard this before?",
+  movie: "I've watched this before?",
+  tv: "I've watched this before?",
+};
