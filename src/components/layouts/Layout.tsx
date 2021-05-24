ba8fe7d9eb17c84a14153e22c72d2fd45c9ab56c
snesjhon <snesjhon@gmail.com>
2021-05-23 17:37:24 -0700

    feat: Update packages and update chakra-containers

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -4,7 +4,7 @@ import { Box, Container } from "@chakra-ui/react";
 
 function Layout({ children }: PropsWithChildren<unknown>): JSX.Element {
   return (
-    <Container maxWidth={{ base: "xl", md: "lg" }}>
+    <Container maxWidth={{ md: "container.lg" }}>
       <Box mx="auto">{children}</Box>
     </Container>
   );
