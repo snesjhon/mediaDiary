7265504f738786810412253afb781a2f27199245
snesjhon <snesjhon@gmail.com>
2021-07-07 22:56:36 -0700

    chore: remove comments

--- src/utils/test-utils/mountWith.tsx
+++ src/utils/test-utils/mountWith.tsx
@@ -6,12 +6,6 @@ import React, { Suspense } from "react";
 import { ContentDrawer } from "../../components/Content/components";
 import MdLoader from "../../components/md/MdLoader";
 
-import {
-  ContextDispatch,
-  ContextState,
-  Reducer,
-} from "../../../src/config/store";
-
 Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: jest.fn().mockImplementation((query) => ({
@@ -34,21 +28,10 @@ export const mountWithContext = createMount({
 export const mountWithDrawerSuspense = createMount({
   context: (options) => options,
   render: (element) => (
-    // <ContextState.Provider
-    //   value={{
-    //     preference: null,
-    //     view: "md",
-    //     diaryFilters: null,
-    //     bookmarkFilters: null,
-    //   }}
-    // >
-    //   <ContextDispatch.Provider value={() => null}>
     <ChakraProvider>
       <ContentDrawer isOpen={true} placement="right">
         <Suspense fallback={<MdLoader />}>{element}</Suspense>
       </ContentDrawer>
     </ChakraProvider>
-    //   </ContextDispatch.Provider>
-    // </ContextState.Provider>
   ),
 });
