bac102540b8248273e513bdaf5d4be13ef05c37e
snesjhon <snesjhon@gmail.com>
2021-10-13 17:44:57 -0400

    feat: Add sidebar sorting for mobile

--- src/components/Diary/components/DiaryHeader/DiaryHeader.tsx
+++ src/components/Diary/components/DiaryHeader/DiaryHeader.tsx
@@ -1,17 +1,11 @@
-import { useMDState } from "@/config";
 import { FiltersIcon, GridIcon, ListIcon } from "@/icons";
 import Filters from "@/src/components/Filters";
-import { ChevronDownIcon, QuestionOutlineIcon } from "@chakra-ui/icons";
+import { useIsBreakpoint } from "@/utils";
+import { QuestionOutlineIcon } from "@chakra-ui/icons";
 import { Box, Flex, Heading, HStack } from "@chakra-ui/layout";
 import {
-  Button,
+  Divider,
   IconButton,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItemOption,
-  MenuList,
-  MenuOptionGroup,
   Text,
   Tooltip,
   useColorMode,
@@ -19,6 +13,7 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import type { SortType, ViewOptions } from "../../config";
+import { DiarySort, DiarySortMobile } from "./components";
 
 interface Props {
   sortType: SortType;
@@ -32,19 +27,8 @@ export default function DiaryHeader({
   view,
 }: Props): JSX.Element {
   const { isOpen, onClose, onOpen } = useDisclosure();
-  const { diaryFilters } = useMDState();
+  const isMd = useIsBreakpoint("md");
   const { colorMode } = useColorMode();
-  const hasFilters = [
-    !!diaryFilters?.diaryYear,
-    !!diaryFilters?.genre,
-    !!diaryFilters?.loggedBefore?.toString(),
-    !!diaryFilters?.mediaType,
-    !!diaryFilters?.rating,
-    !!diaryFilters?.releasedDecade,
-    !!diaryFilters?.releasedYear,
-  ];
-  const hasFiltersCount = hasFilters.filter(Boolean).length;
-  const filterComma = hasFiltersCount > 1 ? ", " : "";
 
   return (
     <>
@@ -74,81 +58,20 @@ export default function DiaryHeader({
             </Tooltip>
           </Flex>
           <Flex maxW="720px" align="center">
-            <HStack spacing={{ base: 0, sm: 3 }}>
-              <Flex alignItems="baseline">
-                {hasFiltersCount > 0 && diaryFilters && (
-                  <Flex alignItems="center" mr="4">
-                    <Text color="gray.500" fontSize="sm" mr="2">
-                      Filtered By:
-                    </Text>
-                    <Text fontSize="sm" fontWeight="bold">
-                      {
-                        <>
-                          {diaryFilters.diaryYear && `Year${filterComma}`}
-                          {diaryFilters.genre && `Genre${filterComma}`}
-                          {diaryFilters.loggedBefore?.toString() &&
-                            `Logged Before${filterComma}`}
-                          {diaryFilters.mediaType && `Media Type${filterComma}`}
-                          {diaryFilters.rating && `Rating`}
-                        </>
-                      }
-                    </Text>
-                  </Flex>
-                )}
-                <Text color="gray.500" fontSize="sm" mr="-1">
-                  Sorted By:
-                </Text>
-                <Menu closeOnSelect={false}>
-                  <MenuButton as={Button} variant="ghost" size="sm" pr="1">
-                    {sortType.type === "diaryDate" && "Diary Added"}
-                    {sortType.type === "addedDate" && "When Rated"}
-                    <ChevronDownIcon boxSize="6" mb="1" />
-                  </MenuButton>
-                  <MenuList minWidth="240px">
-                    <MenuOptionGroup
-                      defaultValue={sortType.sort}
-                      title="Order"
-                      type="radio"
-                    >
-                      <MenuItemOption
-                        value="asc"
-                        onClick={() => onChange({ ...sortType, sort: "asc" })}
-                      >
-                        Ascending
-                      </MenuItemOption>
-                      <MenuItemOption
-                        value="desc"
-                        onClick={() => onChange({ ...sortType, sort: "desc" })}
-                      >
-                        Descending
-                      </MenuItemOption>
-                    </MenuOptionGroup>
-                    <MenuDivider />
-                    <MenuOptionGroup
-                      title="Sort Type"
-                      type="radio"
-                      defaultValue={sortType.type}
-                    >
-                      <MenuItemOption
-                        value="diaryDate"
-                        onClick={() =>
-                          onChange({ ...sortType, type: "diaryDate" })
-                        }
-                      >
-                        Diary Added
-                      </MenuItemOption>
-                      <MenuItemOption
-                        value="addedDate"
-                        onClick={() =>
-                          onChange({ ...sortType, type: "addedDate" })
-                        }
-                      >
-                        When Rated
-                      </MenuItemOption>
-                    </MenuOptionGroup>
-                  </MenuList>
-                </Menu>
-              </Flex>
+            <HStack spacing={3}>
+              {isMd ? (
+                <DiarySort onChange={onChange} sortType={sortType} />
+              ) : (
+                <DiarySortMobile onChange={onChange} sortType={sortType} />
+              )}
+              <IconButton
+                icon={<FiltersIcon />}
+                aria-label="filter"
+                size="sm"
+                variant="outline"
+                onClick={onOpen}
+              />
+              <Divider orientation="vertical" h="3vh" />
               <IconButton
                 icon={view.options === "list" ? <GridIcon /> : <ListIcon />}
                 aria-label="filter"
@@ -157,13 +80,6 @@ export default function DiaryHeader({
                 onClick={() =>
                   view.onChange(view.options === "list" ? "grid" : "list")
                 }
-              />
-              <IconButton
-                icon={<FiltersIcon />}
-                aria-label="filter"
-                size="sm"
-                variant="outline"
-                onClick={onOpen}
               />
             </HStack>
           </Flex>
